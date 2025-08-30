128f68922d05c781bb3fc66268a91f769e564863
theshankarshandwivedi <shankarshan20sept@gmail.com>
2025-08-30 07:37:38 +0530

    initial commit

---
+++ back/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, (req, res) => {
+    app.get("/", (req, res)=> {
+        res.send("CodeForge is running");
+    })
+    console.log(`Server is running on port ${port}`);
+})
