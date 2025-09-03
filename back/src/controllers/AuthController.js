cef9cd8d7de9d1252fda76cd14f94b4239537aad
theshankarshandwivedi <shankarshan20sept@gmail.com>
2025-09-04 01:40:11 +0530

    Add registerUser function

--- back/src/controllers/AuthController.js
+++ back/src/controllers/AuthController.js
@@ -1,6 +1,30 @@
+const User = require("../models/User");
 
-const registerUser = (req, res) => {
-    // Registration logic here
+const registerUser = async (req, res) => {
+
+    try{
+        const [name, username, email, password] = req.body;
+
+        if(!name || !username || !email || !password) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+
+        // Registration logic here
+        const user = { name, username, email, password };
+
+        const response = await User.create(user);
+
+        return res.status(200).json({
+            success: "true",
+            message: "User registered successfully"
+        })
+    } catch (error) {
+        console.error("Error registering user:", error);
+        return res.status(500).json({
+            success: "false",
+            message: "Internal server error"
+        });
+    }
 }
 
 const login = (req, res) => {
